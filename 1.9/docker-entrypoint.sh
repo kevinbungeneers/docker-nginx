fafc23d2f2c7cb73bf2b81a3ece4c8c2eece5e02
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2016-02-11 23:00:09 +0100

    Added APPLICATION_DEBUG environment variable

--- 1.9/docker-entrypoint.sh
+++ 1.9/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/nginx/conf.d/default.conf \
 && sed -i "s|INDEX_FILE|$INDEX_FILE|g" /etc/nginx/conf.d/default.conf \
 && sed -i "s|FASTCGI_PASS|$FASTCGI_PASS|g" /etc/nginx/conf.d/default.conf \
-&& sed -i "s|APP_ENV|$APP_ENV|g" /etc/nginx/conf.d/default.conf
+&& sed -i "s|APP_ENV|$APP_ENV|g" /etc/nginx/conf.d/default.conf \
+&& sed -i "s|APP_DEBUG|$APP_DEBUG|g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
